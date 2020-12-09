957c36119a283d5ae1c9e1467d9344aa286b1535
JacooobK9 <jacooobk9@gmail.com>
2020-12-09 06:02:02 +0100

    Commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        animator.SetFloat("vertical", Input.GetAxis("Vertical"));
+        animator.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            Jump();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift));
+
+
+    }
+
+    public void Jump()
+    {
+        animator.SetTrigger("Jump");
+    }
+    public void Run()
+    {
+        animator.SetBool("Run", true);
+    }
+}
+
+

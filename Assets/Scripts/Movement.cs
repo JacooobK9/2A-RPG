30dab4d0a2ae65c440e210d712c8dd2867b3e56b
JacooobK9 <jacooobk9@gmail.com>
2021-01-25 04:14:41 +0100

    Animacje

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,21 +5,64 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     Animator animator;
+    bool isRunning = false;
+    [SerializeField] GameObject[] skillParticles;
+    private IEnumerator coroutine;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        for (int i = 0; i < skillParticles.Length; i++)
+        {
+            skillParticles[i].SetActive(false);
+        }
     }
 
     void Update()
     {
         animator.SetFloat("vertical", Input.GetAxis("Vertical"));
         animator.SetFloat("horizontal", Input.GetAxis("Horizontal"));
-        if(Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space)) Jump();
+        if (Input.GetKeyUp(KeyCode.LeftShift)) Run(false);
+        if (Input.GetKeyDown(KeyCode.LeftShift)) Run(true);
+        if (Input.GetKeyUp(KeyCode.Alpha1)) UseSkill(1);
+        if (Input.GetKeyUp(KeyCode.Alpha2)) UseSkill(2);
+        if (Input.GetKeyUp(KeyCode.Alpha3)) UseSkill(3);
+        if (Input.GetKeyUp(KeyCode.Alpha4)) UseSkill(4);
+        if (Input.GetKeyUp(KeyCode.Alpha5)) UseSkill(5);
+    }
+
+    void UseSkill(int skillNumber)
+    {
+        animator.SetTrigger("Skill" + skillNumber);
+        skillParticles[skillNumber - 1].SetActive(true);
+
+        switch(skillNumber)
         {
-            Jump();
+            case 1:
+                coroutine = WaitToEnableObject(skillParticles[skillNumber - 1], 2);
+                break;
+            case 2:
+                coroutine = WaitToEnableObject(skillParticles[skillNumber - 1], 2.5f);
+                break;
+            case 3:
+                coroutine = WaitToEnableObject(skillParticles[skillNumber - 1], 2.5f);
+                break;
+            case 4:
+                coroutine = WaitToEnableObject(skillParticles[skillNumber - 1], 2.5f);
+                break;
+            case 5:
+                coroutine = WaitToEnableObject(skillParticles[skillNumber - 1], 2.5f);
+                break;
         }
-        if (Input.GetKeyUp(KeyCode.LeftShift));
+
+        StartCoroutine(coroutine);
+    }
+
+    IEnumerator WaitToEnableObject(GameObject obj, float time)
+    {
+        yield return new WaitForSeconds(time);
+        obj.SetActive(false);
 
 
     }
@@ -28,9 +71,10 @@ public class Movement : MonoBehaviour
     {
         animator.SetTrigger("Jump");
     }
-    public void Run()
+    public void Run(bool startMovement)
     {
-        animator.SetBool("Run", true);
+        isRunning = startMovement;
+        animator.SetBool("isRunning", isRunning);
     }
 }
 
